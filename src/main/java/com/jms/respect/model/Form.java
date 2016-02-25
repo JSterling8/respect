a5321d2d0bf9d6427220b29537ae39cc24b1acd2
JSterling8 <JSterling8@gmail.com>
2016-02-25 14:00:15 +0000

    Added in empty model and skeleton class

---
+++ src/main/java/com/jms/respect/model/Form.java
@@ -0,0 +1,10 @@
+package com.jms.respect.model;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by anon on 25/02/2016.
+ */
+@Entity
+public class Form {
+}
---
+++ src/main/java/com/jms/respect/service/FormService.java
@@ -0,0 +1,10 @@
+package com.jms.respect.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by anon on 25/02/2016.
+ */
+@Service
+public class FormService {
+}
