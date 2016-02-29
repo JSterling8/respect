3fef3826c3d8cffc6a88ea34cda803b3d4eba172
JSterling8 <JSterling8@gmail.com>
2016-02-29 23:55:41 +0000

    Added basic authentication for one in-memory user.  Form 1-10 now using loop

---
+++ src/main/java/com/jms/respect/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.jms.respect.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/", "/home").permitAll()
+                    .antMatchers("/webjars/*", "/img/*").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    .failureUrl("/login?error")
+                    .defaultSuccessUrl("/form", true)
+                    .permitAll()
+                .and()
+                    .logout()
+                    .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user@user.com").password("password").roles("USER");
+    }
+}
--- src/main/java/com/jms/respect/controller/LoginController.java
+++ src/main/java/com/jms/respect/controller/LoginController.java
@@ -1,7 +1,26 @@
 package com.jms.respect.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by anon on 25/02/2016.
  */
+@Controller
 public class LoginController {
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String getLoginPage() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String handleLogin(@RequestParam(value = "email") String email, @RequestParam(value = "pass") String pass, HttpServletRequest request) {
+        String referer = request.getHeader("referer");
+
+        return "login";
+    }
 }
