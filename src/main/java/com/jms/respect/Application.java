3d418f28e14384838e19ff50c415adab5bb0d413
JSterling8 <JSterling8@gmail.com>
2016-03-17 13:25:07 +0000

    Added todo/fixme

--- src/main/java/com/jms/respect/Application.java
+++ src/main/java/com/jms/respect/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+//FIXME - IMPLEMENT CACHING!
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
     }
